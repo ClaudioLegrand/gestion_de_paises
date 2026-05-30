305aab85f40979a952aadb1df9bb351d6c529e2f
Claudio <xlaudiolegrand@gmail.com>
2026-05-30 13:53:12 -0300

    realizando la funcion de agragar paises

--- sistemagestiondepaises.py
+++ sistemagestiondepaises.py
@@ -38,16 +38,66 @@ def cargardatos(archivo):
                 dirpaises = dict(zip(encabezado, partes))
                 paises.append(dirpaises)
 
-# funcion para guardar datos en el archivo csv
-# def guardar_datos(paises, archivo):
-#     with open(archivo, "w", encoding="utf-8") as f:
-#         if paises:
-#             f.write(",".join(paises[0].keys()) + "\n")
-#             for pais in paises:
-#                 f.write(",".join(pais.values()) + "\n")
-#     area = input("Ingrese el área del país (en km²): ")
-#     idioma = input("Ingrese el idioma oficial del país: ")
+    return paises
 
+# funcion para guardar los datos del país en la lista de diccionarios
+def guardar_datos(paises):
+
+    # ---------------------------------------------------------------- preguntar cuantos paises quieren agregar?
+
+    nombre_repetido = True
+    intentos = 3 #----------------------------------------------------- vemos si ponemos intentos
+
+    # verificamos de que no ingrese un país que ya existe en el sistema
+    while nombre_repetido and intentos > 0:
+
+        # pedimos el nombre del país
+        nombre = input("Ingrese el nombre del país para agregar: ").strip().lower()
+
+        nombre_repetido = False
+        for p in paises:
+            if p["nombre"] == nombre:
+                print("El país ya existe en el sistema. ingrese un país diferente.")
+                nombre_repetido = True
+                break
+
+    # si se queda sin intentos, volvemos al menú principal
+    if intentos == 0:
+        print("Se han agotado los intentos. Volviendo al menú principal.")
+        return
+
+    poblacion = input("Ingrese la población del país: ").strip()
+    superficie = input("Ingrese la superficie del país: ").strip()
+    continente = input("Ingrese el continente del país: ").strip().lower()
+    # ---------------------------------------------------------------------- falta mejorar validaciones
+
+    # creamos un diccionario para el país y lo agregamos a la lista de países
+    pais = {
+        "nombre": nombre,
+        "poblacion": poblacion,
+        "superficie": superficie,
+        "continente": continente
+    }
+
+    # lo apregamos a la lista de países
+    paises.append(pais)
+
+
+# ------------------------------------------------------------------ funcion solo para agregar?
+# funcion para agregar datos al archivo csv
+def añadir_datos_archivo(paises, archivo):
+    try:
+        with open(archivo, "a", encoding="utf-8") as f:
+            for p in paises:
+                linea = f"{p['nombre']},{p['poblacion']},{p['superficie']},{p['continente']}\n"
+                f.write(linea)
+
+    except FileNotFoundError:
+        print("Error: El archivo no existe. No se pudieron guardar los datos.")
+    except IOError:
+        print("Error: No se pudieron guardar los datos en el archivo.") #---------------------- ver si se puede usar
+    except Exception as e:
+        print(f"Error inesperado: {e}")
 
 
 # funcion que valida numero enteros
@@ -89,8 +139,14 @@ while opcion != 8:
     try:
         match opcion:
             case 1:
-                print("AGREGAR PAIS")
-                #agregar_pais(paises)
+
+                # pedimos los datos del país al usuario y los guardamos en un diccionario
+                guardar_datos(paises)
+
+                # guardamos el diccionario en el archivo csv
+                añadir_datos_archivo(paises, archivopaises)
+                print("País agregado y guardado correctamente!")
+
             case 2:
                 print("ACTUALIZAR DATOS DEL PAIS")
                 #actualizar_pais(paises)    
