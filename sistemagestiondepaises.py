739a36113e5f3c8550b0803f95f0612c6273c7d8
christianherrero98 <christianherreroecom@gmail.com>
2026-05-31 00:28:15 +0200

    Actualizacion de carga de datos + actualizar pais + verificar si numero ok
    Se arregla problema de lista de diccionarios que faltaba un return en la funcion.
    
    Se crea la funcion actualizardatos
    
    se crea la funcion para verificar si numero agergado esta ok para poblacion y superficie

--- sistemagestiondepaises.py
+++ sistemagestiondepaises.py
@@ -20,6 +20,10 @@ def menu():
         ------------------------------------------
         ''')  
     
+# --------------------------------------------------
+#                   CARGA DE ARCHIVO
+# --------------------------------------------------    
+    
 def cargardatos(archivo):
 #cargar datos desde el archivo csv  
 #retorna una lista de diccionarios
@@ -37,26 +41,69 @@ def cargardatos(archivo):
                 dirpaises = dict(zip(encabezado, partes))
                 paises.append(dirpaises)
         print(paises)
+        return paises #ESTO LO AGREGUE POR QUE NO DEVOLVIA LA LISTA DE DICCIONARIOS FUERA DE LA FUNCION
+        
+# --------------------------------------------------
+#              ACTUALIZAR DATOS DE PAIS
+# --------------------------------------------------
+def actualizardatospais(pais2es):
+    print("""
+        ------------------------------------------
+                    ACTUALIZAR PAIS                      
+        ------------------------------------------
+        """)
+    nombrepais=input("Ingrese nombre de pais a actualizar: ").strip().lower()
+    #verificar si pais existe
+    for pais in paises:
+        if pais["nombre"].lower() == nombrepais:
+            #QUE SE MUESTRE ACTUALIZAR POBLACION: [SI NO QUIERE ACTUALIZAR PRESIONE "ENTER"]
+            #QUE PASA SI TIENE ACENTOS? COMO HAGO PARA QUE NO LOS TENGA EN CUENTA?
+            try:
+                print(f"Datos actuales de -- {pais["nombre"]} --\n")
+                print(f"Población: {pais["poblacion"]} | Superficie: {pais["superficie"]}")
+                nuevapoblacion=input("Ingrese la nueva población [Enter para mantener la actual]\n").strip()
+                nuevasuperficie=input("Ingrese la nueva superficie [Enter para mantener la actual]\n").strip()
+                if nuevapoblacion:
+                    pais["poblacion"]=validar_numero_correcto(nuevapoblacion,"poblacion")
+                if nuevasuperficie:
+                    pais["superficie"]=validar_numero_correcto(nuevasuperficie, "superficie")
+                print(f"Los datos de {pais["nombre"]} fueron actulizados correctamente")    
+            except ValueError as e:
+                print(f"Error: Valor ingresado es incorrecto -> {e}")
+                return
+    else: print("Error: Pais no encontrado [puede cargar un país en la opción 1]")
+            
+def validar_numero_correcto(vnc_nuevapoblacion, variablearevisar):
+    numero_de_poblacion= int(vnc_nuevapoblacion)
+    try:
+        if numero_de_poblacion <=0:
+            raise ValueError #Lanza error si pasa esto (habilita el except del valuerror)
+        return numero_de_poblacion   
+    except ValueError:
+        raise ValueError(f"Error: El valor ingresado debe ser un valor entero positivo")
+        
     
+        
+          
 # --------------------------------------------------
 #                  MAIN DEL SISTEMA
 # --------------------------------------------------
-
 print("\nCargando datos...")
 #carga de datos del archivo en variable
 ruta_actual = os.path.dirname(__path__ if '__path__' in locals() else __file__)
 archivopaises= os.path.join(ruta_actual, "datos/paises.csv")
 paises=cargardatos(archivopaises)
+print(f"PRINT DE [PAISES] FUERA DE FUNCION: {paises}")
 print("Datos cargados correctamente!")
 
 while True:
     menu()
-    opcion=input("Ingrese una opción numerica [1-8]: ")
+    opcion=input("Ingrese una opción numerica [1-8]: ").strip()
     try:
         if opcion== "1":
             pass
         elif opcion== "2":
-            pass
+            actualizardatospais(paises)
         elif opcion== "3":
             pass
         elif opcion== "4":
